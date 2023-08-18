01ae3142ce3944bc6721e50e2d404ae0e1c59b60
Pepijn van Wijk <pvwpvw@protonmail.com>
2023-08-18 13:37:47 +0400

    Updated to support local inference

--- PastorGPT.py
+++ PastorGPT.py
@@ -1,26 +1,49 @@
+import argparse
 from dotenv import load_dotenv
 import faiss
+import json
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import LlamaCpp
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 import os
 import pickle
-import textract
+from pdfminer.high_level import extract_text
+import torch
 
+
+# Parser to handle the model argument.
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local",
+                        action="store_true",
+                        help="Run a local embedder and chat model in stead of the OpenAI api.")
+    parser.add_argument("--save",
+                        action="store_true",
+                        help="Save chat history, including the source of the answer to a local json file.")
+
+    local = parser.parse_args().local
+    save = parser.parse_args().save
+    return local, save
 
 class Pastor:
-    def __init__(self, pdf_file="bible.pdf", txt_file="bible.txt", db_name="bible.pkl"):
+    def __init__(self, local, save_convo, pdf_file="bible.pdf", txt_file="bible.txt", db_name="bible.pkl"):
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
         load_dotenv()
         # Checks for openAI API key.
         if os.environ.get('OPENAI_API_KEY') is None:
-            print("Please provide a valid OpenAI API key in the .env file. See .env.example for more information")
-            exit(1)
+            print("Please provide a valid OpenAI api key in the .env file. See .env.example for more information")
+            raise Exception("No OpenAI api key")
         self.pdf_file = pdf_file
         self.txt_file = txt_file
         self.db_name = db_name
+        self.local = local
+        self.save_convo = save_convo
+        self.save_path = None
 
         if not os.path.isfile(self.txt_file):
             print(f"Creating {self.txt_file}...")
@@ -32,29 +55,98 @@ class Pastor:
             self.create_faiss_db(chunks)
 
         self.load_faiss_db()
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        self.qa_chain = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.05), self.db.as_retriever(), memory=memory)
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True,
+                                          input_key='question', output_key='answer')
+        chatTemplate = """
+                       Answer the question based on the chat history(delimited by <hs></hs>) and context(delimited by <ctx> </ctx>) below. If you don't know the answer based on the given context, just say that you don't know, don't try to make up an answer. Keep the answers as concise as possible, but do explain your answer by quoting a piece of the context.
+                       -----------
+                       <ctx>
+                       {context}
+                       </ctx>
+                       -----------
+                       <hs>
+                       {chat_history}
+                       </hs>
+                       -----------
+                       Question: {question}
+                       Answer:
+                       """
+        templatePrompt = PromptTemplate(input_variables=["context", "question", "chat_history"], template=chatTemplate)
+        llm = self.setup_llm()
+        self.qa_chain = ConversationalRetrievalChain.from_llm(llm,
+                                                              self.db.as_retriever(),
+                                                              memory=memory,
+                                                              return_source_documents=self.save_convo,
+                                                              combine_docs_chain_kwargs={'prompt': templatePrompt} if local else None)
+
+
+    # Sets up the llm, depending on the use of the '--local' flag.
+    def setup_llm(self):
+        callbacks = []
+        if self.local:
+            if os.environ.get('MODEL_PATH') is None or not os.path.isfile(os.environ.get('MODEL_PATH')):
+                print("Please specify a valid model path that is to be used for local inference.")
+                raise Exception("No model specified")
+            return LlamaCpp(model_path=os.path.abspath(os.environ.get('MODEL_PATH')),
+                            max_tokens=8192,
+                            n_ctx=2048,
+                            n_gpu_layers=1,
+                            f16_kv = True,
+                            n_batch = 512,
+                            verbose=False, # Info about time taken to generate answer.
+                            callbacks=callbacks)
+
+        return OpenAI(temperature=0.05)
 
 
     def run(self):
         print("Starting chat. Type 'q' or 'exit' to quit.")
         while True:
             query = input("Ask your question: ")
-            if not query:
+            if not query or query.strip() == "":
                 continue
             if query == "q" or query == "exit":
+                self.finish_save_file()
                 return
             res = self.search(query)
-            print(res)
+            ans, src = res["answer"], [] if not self.save_convo else res['source_documents']
+            print(f"\t{ans}")
+            if self.save_convo:
+                self.save_conversation(query, ans, src)
             query = ""
 
 
+    # Saves the conversation by writing to a json file.
+    def save_conversation(self, query, answer, sources):
+        if self.save_path is None:
+            i = len([f for f in os.listdir("conversations/") if f.startswith("bible")])
+            self.save_path = f"conversations/bible-conversation{i:02d}.json"
+            with open(self.save_path, "a") as f:
+                conv = {"question": query, "answer": answer, "sources": [doc.page_content for doc in sources]}
+                f.write("[" + json.dumps(conv))
+            return
+
+        with open(self.save_path, "a") as f:
+            conv = {"question": query, "answer": answer, "sources": [doc.page_content for doc in sources]}
+            f.write("," + json.dumps(conv))
+
+
+    # Closes the json array.
+    def finish_save_file(self):
+        if self.save_path is None:
+            return
+        with open(self.save_path, "a") as f:
+            f.write("]")
+
+
+    # Converts pdf file to txt file.
     def parse_pdf(self):
         text = textract.process(self.pdf_file)
         with open(self.txt_file, 'w+') as f:
             f.write(text.decode('utf-8'))
 
 
+    # Splits txt file into chunks.
     def split_txt(self):
         with open(self.txt_file, "r") as f:
             text = f.read()
@@ -67,14 +159,21 @@ class Pastor:
         return chunks
 
 
+    # Creates and saves Faiss db from the chunks generated by split_txt().
     def create_faiss_db(self, chunks):
-        store = FAISS.from_texts(chunks, OpenAIEmbeddings())
-        faiss.write_index(store.index, "chunks.index")
-        store.index = None
+        if self.local:
+            print(f"Setting up local embedder using {'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'}")
+            db = FAISS.from_texts(chunks, HuggingFaceEmbeddings(model_name='sentence-transformers/multi-qa-MiniLM-L6-cos-v1',
+                                                                model_kwargs={'device': 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'})) # model_name="all-MiniLM-L6-v2"
+        else:
+            db = FAISS.from_texts(chunks, OpenAIEmbeddings())
+        faiss.write_index(db.index, "chunks.index")
+        db.index = None
         with open(self.db_name, "wb") as f:
-            pickle.dump(store, f)
+            pickle.dump(db, f)
 
 
+    # Loads Faiss db into memory.
     def load_faiss_db(self):
         index = faiss.read_index("chunks.index")
 
@@ -85,10 +184,11 @@ class Pastor:
         self.db = db
 
 
+    # Searches for the query in the Faiss db.
     def search(self, query):
-        result = self.qa_chain({"question": query})
-        return result["answer"]
+        return self.qa_chain({"question": query})
 
 
-p = Pastor()
+local, save = parse_args()
+p = Pastor(local, save)
 p.run()
--- README.md
+++ README.md
@@ -11,6 +11,21 @@ Next, you need an [OpenAI api key](https://platform.openai.com/overview). Add th
 ```
 python3 PastorGPT.py
 ```
+With parameters:
+```
+-h, --help            show this help message and exit
+--local               Run a local embedder and chat model in stead of the OpenAI api.
+--save                Save chat history, including the source of the answer, to a local json file.
+```
+
+# Local inference
+Besides using OpenAI's Davinci-003 model, local inference is also supported. For local inference to work, you'll need [llama.cpp](https://github.com/ggerganov/llama.cpp) installed and you will need valid GGML model weights placed in the `models/` directory. These models can be downloaded [here (7B)](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main), [here (13B)](https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/tree/main) or [here (70B)](https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGML/tree/main). You can also [request access](https://ai.meta.com/resources/models-and-libraries/llama-downloads/) and convert the weights to GGML yourself. Keep in mind that the llama chat model weights are needed, not the regular weights.
+
+# Similar projects
+I made some other projects based on this project. These other projects do mostly the same thing, but the documents are static and focussed on a specific niche.
+- [pdfGPT](https://github.com/deboradum/pdfGPT), chat with any pdf to your choosing.
+- [uvaGPT](https://github.com/deboradum/uvaGPT), chat with CS textbooks used at the University of Amsterdam.
+- [imamGPT](https://github.com/deboradum/ImamGPT), chat with the Quran.
 
 # Demo
 .
